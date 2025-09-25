87659a22aa23ca054f2c21f0cf366e8b75b3075e
Агаджанян Артём Робертович <10230278@college.local>
2025-09-25 12:26:36 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,24 @@
+fun main() {
+    println("Введите натуральное число:")
+    val input = readlnOrNull() ?: ""
+
+    val number = input.toIntOrNull()
+
+    if (number == null || number < 0) {
+        println("Ошибка: введите корректное натуральное число!")
+        return
+    }
+    if (number == 0) {
+        println("Двоичное представление: 0")
+        return
+    }
+    var currentNumber = number
+    val binaryDigits = mutableMapOf<Int>()
+
+    while (currentNumber > 0) {
+        val remainder = currentNumber % 2
+        binaryDigits.add(remainder)
+        currentNumber = currentNumber / 2
+
+    }
+}
